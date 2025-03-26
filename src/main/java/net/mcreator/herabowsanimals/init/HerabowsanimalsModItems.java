e8afffb55f380857eca12ae489058d1e38984978
EnnyTheDonkey <Romek@Romek-Komputer>
2025-03-26 22:01:11 +0100

    Initial build: - Added Reptile skin + armor - Added folders for the first 3 mobs(reptiles) - Added Crab Item texture - thinking about making it a grabbable mob - Added Creative Tab

---
+++ src/main/java/net/mcreator/herabowsanimals/init/HerabowsanimalsModItems.java
@@ -0,0 +1,25 @@
+
+/*
+ *    MCreator note: This file will be REGENERATED on each build.
+ */
+package net.mcreator.herabowsanimals.init;
+
+import net.neoforged.neoforge.registries.DeferredRegister;
+import net.neoforged.neoforge.registries.DeferredItem;
+
+import net.minecraft.world.item.Item;
+
+import net.mcreator.herabowsanimals.item.ReptileSkinItem;
+import net.mcreator.herabowsanimals.item.ReptileArmorItem;
+import net.mcreator.herabowsanimals.HerabowsanimalsMod;
+
+public class HerabowsanimalsModItems {
+	public static final DeferredRegister.Items REGISTRY = DeferredRegister.createItems(HerabowsanimalsMod.MODID);
+	public static final DeferredItem<Item> REPTILE_SKIN = REGISTRY.register("reptile_skin", ReptileSkinItem::new);
+	public static final DeferredItem<Item> REPTILE_ARMOR_HELMET = REGISTRY.register("reptile_armor_helmet", ReptileArmorItem.Helmet::new);
+	public static final DeferredItem<Item> REPTILE_ARMOR_CHESTPLATE = REGISTRY.register("reptile_armor_chestplate", ReptileArmorItem.Chestplate::new);
+	public static final DeferredItem<Item> REPTILE_ARMOR_LEGGINGS = REGISTRY.register("reptile_armor_leggings", ReptileArmorItem.Leggings::new);
+	public static final DeferredItem<Item> REPTILE_ARMOR_BOOTS = REGISTRY.register("reptile_armor_boots", ReptileArmorItem.Boots::new);
+	// Start of user code block custom items
+	// End of user code block custom items
+}
---
+++ src/main/java/net/mcreator/herabowsanimals/item/ReptileSkinItem.java
@@ -0,0 +1,11 @@
+
+package net.mcreator.herabowsanimals.item;
+
+import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.Item;
+
+public class ReptileSkinItem extends Item {
+	public ReptileSkinItem() {
+		super(new Item.Properties().stacksTo(64).rarity(Rarity.COMMON));
+	}
+}
